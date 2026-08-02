bc44665fddd4c2afb9b7fbf1769f819b6b86cdc1
Sahil Patil <patilsahil@172002@gmail.com>
2026-08-02 19:48:35 +0530

    fix: update API baseURL to use environment variable for better configuration

--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:8000/api",
-    // import.meta.env.VITE_API_URL 
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true
 });
 
