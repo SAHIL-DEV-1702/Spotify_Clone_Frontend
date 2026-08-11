fd2c4e4ec61e8611732d5140a340c38284814954
Sahil Patil <patilsahil@172002@gmail.com>
2026-08-11 15:35:55 +0530

    adjust loader size

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
     return (
         <div className="flex h-40 items-center justify-center">
-            <div className="h-6 w-6 animate-spin rounded-full border-6 border-zinc-600 border-t-green-500"></div>
+            <div className="h-10 w-10 animate-spin rounded-full border-4 border-zinc-600 border-t-green-500"></div>
         </div>
     );
 };
